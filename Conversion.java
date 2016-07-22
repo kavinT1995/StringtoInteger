22723ca6695fcafbbe048c632a803307ab5e6105
kavinT1995 <kavin.tcse@ksrce.ac.in>
2016-07-22 14:12:31 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,15 @@
+# StringtoInteger
+public class Conversion {
+
+	
+	public static void main(String[] args) {
+		String str="java";
+		StringBuffer sr=new StringBuffer(str);
+		for(int i=0;i<sr.length();i++){
+			int c=sr.charAt(i);
+			System.out.println(c);
+		}
+		
+	}
+
+}
